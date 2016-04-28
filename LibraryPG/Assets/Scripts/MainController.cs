6f68f0eb5941719d4c6b11d56c1d4becf9b172f9
QuiX23 <rav.sadowwski@gmail.com>
2016-04-28 21:53:15 +0200

    walking warking

--- LibraryPG/Assets/Scripts/MainController.cs
+++ LibraryPG/Assets/Scripts/MainController.cs
@@ -13,20 +13,16 @@ public class MainController : MonoBehaviour
 
     public static MainController GetInstance()
     {
-        if (instance == null)
-        {
-            instance = new MainController();
-        }
-
-        return instance;
+        return instance ?? (instance = new MainController());
     }
+
     #endregion
 
     public Location currentLocation;
 
     public List<Location> locations;
     public GameObject arrowPrefab;
-    public GameObject arrows;
+    public RectTransform arrows;
 
     public float arrowDistance = 100;
 
@@ -34,26 +30,38 @@ public class MainController : MonoBehaviour
     {
         currentLocation = location;
         RenderSettings.skybox = currentLocation.skybox;
-        LoadNeighbours(location);
+        SetNeighbours(location);
     }
 
     public void Start()
     {
+        instance = this;
         ChangeLocation(currentLocation);
     }
 
-    void LoadNeighbours(Location location)
+    void SetNeighbours(Location location)
+    {
+        DestroyNeighbours(location);
+        LoadNeighbours(location);
+    }
+
+    void DestroyNeighbours(Location location)
     {
         if (arrows == null) return;
-        foreach (Transform arrow in arrows.transform)
+
+        foreach (RectTransform arrow in arrows)
         {
             Destroy(arrow.gameObject);
         }
+    }
+    void LoadNeighbours(Location location)
+    {
+
         foreach (Neighbour neighbour in location.neighbours)
         {
             GameObject go = Instantiate(arrowPrefab);
 
-            go.transform.parent = arrows.transform;
+            go.transform.parent = arrows;
 
             go.transform.position = Vector3.Normalize(neighbour.direction)* arrowDistance;
             go.transform.LookAt(new Vector3(0, transform.position.y, 0));
