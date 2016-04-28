cd583e79f70fef681aa3e24ab544beae22e73db9
Tomasz Rozwadowski <Tomasz Rozwadowski>
2016-04-28 22:59:07 +0200

    Fixed blocking coursor touch

--- LibraryPG/Assets/Scripts/BlockMouse.cs
+++ LibraryPG/Assets/Scripts/BlockMouse.cs
@@ -1,32 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.Characters.FirstPerson;
-
+using UnityEngine.EventSystems;
 public class BlockMouse : MonoBehaviour
+
 {
-    private FirstPersonController controller;    // Use this for initialization
+    private FirstPersonController controller;
+    public bool isOver = false;
+
     void Start ()
     {
         controller = GetComponent<FirstPersonController>();
-
     }
-	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButton(0))
         {
-            Screen.lockCursor = true;
-            Debug.Log("coo1");
+            if (isOver = false)
+                Screen.lockCursor = true;
+            else
+                Screen.lockCursor = false;
             controller.m_MouseLook.XSensitivity = 2f;
             controller.m_MouseLook.YSensitivity = 2f;
         }
         else
         {
             Screen.lockCursor = false;
-
-            Debug.Log("coo2");
             controller.m_MouseLook.XSensitivity = 0f;
             controller.m_MouseLook.YSensitivity = 0f;
         }
+    }
+
+    public void OnPointerEnter()
+    {
+        Debug.Log("Mouse enter");
+        isOver = true;
+    }
+
+    public void OnPointerExit()
+    {
+        Debug.Log("Mouse exit");
+        isOver = false;
     }
 }
